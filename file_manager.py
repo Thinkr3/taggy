03f16f4f661ed8921c08beaadf98fff6c820ce53
Matthew Brownrigg <45981739+Thinkr3@users.noreply.github.com>
2023-07-18 19:02:40 -0400

    Add 'all sort' and 'popular sort' functionality

--- file_manager.py
+++ file_manager.py
@@ -1,23 +1,72 @@
 import os
+import shutil
 import filetype as ft
 from exif_editor import ExifEditor
 
 
-folder_path: str = ""
-keys: dict = {}
+# folder_path: str = ""
 
-def keyword_frequency(folderpath: str = "") -> dict:
-    for file in os.listdir(folder_path if folder_path != "" else os.getcwd()):
+def keyword_frequency(folderpath: str = os.getcwd()) -> dict:
+    keys: dict = {}
+    for file in os.listdir(folderpath):
         if os.path.isfile(file) and ft.image_match(file) != None:
             img = ExifEditor(file)
-            print(img.keywords)
+            # print(img.keywords)
             for keyword in img.keywords:                
                 if keyword in keys:
                     keys[keyword] += 1
                 else:
                     keys[keyword] = 1
             img.close_image()
+    keys = dict(sorted(keys.items(), key=lambda x: x[1], reverse=True))
     return keys
 
+def repeat_all_sort(folderpath: str = os.getcwd(), threshold: int = 0, hardlinks: bool = True) -> None:
+    '''Sorts the images in the folderpath by their keywords. Image is sorted into every keyword.'''
+    keys = keyword_frequency(folderpath)
+    for file in os.listdir(folderpath):
+        if os.path.isfile(file) and ft.image_match(file) != None:
+            img = ExifEditor(file)
+            img.close_image() # Prevents System Error 32 and memory leaks
 
-print(keyword_frequency())
+            # Filters out keywords that don't meet the threshold
+            for keyword in filter(lambda x: keys[x] >= threshold, img.keywords):
+                if not os.path.exists(keyword):
+                    os.mkdir(keyword)
+                if hardlinks:
+                    os.link(file, keyword + "/" + file)
+                else:
+                    shutil.copy(file, keyword + "/" + file)
+
+def most_popular_sort(folderpath: str = os.getcwd(), threshold: int = 0, hardlinks: bool = True) -> None:
+    '''Sorts the images in the folderpath by their most popular keywords. Only sorts the image into most popular keyword.'''
+    keys = keyword_frequency(folderpath)
+    for file in os.listdir(folderpath):
+        if os.path.isfile(file) and ft.image_match(file) != None:
+            img = ExifEditor(file)
+            img.close_image() # Prevents System Error 32 and memory leaks
+            
+            # Filters out keywords that don't meet the threshold
+            reduced_keywords = list(filter(lambda x: keys[x] >= threshold, img.keywords))
+
+            # If there are no keywords that meet the threshold, skip the file
+            if len(reduced_keywords) == 0:
+                continue
+            
+            # Finds the most popular keyword
+            keyword = max(reduced_keywords, key=lambda x: keys[x])
+            
+            if not os.path.exists(keyword):
+                os.mkdir(keyword)
+            if hardlinks:
+                os.link(file, keyword + "/" + file)
+            else:
+                shutil.copy(file, keyword + "/" + file)
+
+def populate_dirs(folderpath: str = os.getcwd()) -> None:
+    keys = keyword_frequency(folderpath)
+    for key in keys:
+        if not os.path.exists(key):
+            os.mkdir(key)
+
+most_popular_sort(threshold=1)
